0039b40d1ed4bcb933dbacdb4c446013e5d93bd1
Alok Debnath <alokdebnath5@gmail.com>
2020-06-09 10:24:10 +0200

    data preprocessing completed

--- Experiments/models/preprocess.py
+++ Experiments/models/preprocess.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
+import io
 from tqdm import tqdm
 import re
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import unicodedata
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
 
 class IxGen:
     def __init__(self):
@@ -44,10 +44,9 @@ def normalizeString(s):
     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
     return s
 
-def readIxGens(datapath):
+def readIxGens(lines):
     print("Reading lines...")
-    lines = open(datapath, encoding='utf-8').read().strip().split('\n')
-    pairs = [[normalizeString(s) for s in l.split('\t')][2:] for l in tqdm(lines)]
+    pairs = [[normalizeString(s) for s in l] for l in tqdm(lines)]
     ixgen = IxGen()
     return ixgen, pairs
 
@@ -70,8 +69,8 @@ def filterPair(p):
 def filterPairs(pairs):
     return [pair for pair in pairs if filterPair(pair)]
 
-def prepareData(datapath):
-    ixgen, pairs = readIxGens(datapath)
+def prepareData(lines):
+    ixgen, pairs = readIxGens(lines)
     print("Read %s sentence pairs" % len(pairs))
     pairs = filterPairs(pairs)
     print("Trimmed to %s sentence pairs" % len(pairs))
@@ -104,3 +103,26 @@ def tensorsFromPair(pair):
     target_tensor = tensorFromSentence(pair[1])
     return (input_tensor, target_tensor)
 
+def read_train_test_dev(batch_size, dev_files, test_files, data_file):
+    dev_X = []
+    test_X = []
+    train_X = []
+    dev_files = set(open(dev_files).read().split())
+    test_files = set(open(test_files).read().split())
+    dev_id, test_id = 0, 0
+    with io.open(data_file, encoding='utf-8') as fp:
+        for line in fp:
+            title, revision_group, src, tgt = line.strip().split('\t')
+            if len(src) > 150 or len(tgt) > 150:
+                continue
+            if title in dev_files:
+                dev_X.append([src, tgt])
+            elif title in test_files:
+                test_X.append([src, tgt])
+            else:
+                train_X.append([src, tgt])
+    return train_X, test_X, dev_X
+
+a, b, c = read_train_test_dev(100, os.path.join('../data', 'dev_files.txt'), os.path.join('../data/', 'test_files.txt'), 
+                                '../data/wikiHow_revisions_corpus.txt')
+prepareData(a)
